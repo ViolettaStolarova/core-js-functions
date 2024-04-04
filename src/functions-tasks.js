cd6d326a467daff3325a650f0e46c89509288e45
Violetta Stolarova <violettastolarova@icloud.com>
2024-04-04 22:12:18 +0300

    feat: add logger function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -192,8 +192,22 @@ function retry(func, attempts) {
  * cos(3.141592653589793) ends
  *
  */
-function logger(/* func, logFunc */) {
-  throw new Error('Not implemented');
+function logger(func, logFunc) {
+  return (...args) => {
+    const argsStr = args
+      .map((arg) =>
+        Array.isArray(arg)
+          ? `[${arg
+              .map((item) => (typeof item === 'number' ? item : `"${item}"`))
+              .join(',')}]`
+          : arg
+      )
+      .join(',');
+    logFunc(`${func.name}(${argsStr}) starts`);
+    const result = func(...args);
+    logFunc(`${func.name}(${argsStr}) ends`);
+    return result;
+  };
 }
 
 /**
