3f3468b9ff1776cd6e78d891e2bee9e786bafc38
Violetta Stolarova <violettastolarova@icloud.com>
2024-04-04 21:54:58 +0300

    feat: add partialUsingArguments function

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -209,8 +209,11 @@ function logger(/* func, logFunc */) {
  *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args2) => {
+    const combinedArgs = [...args1, ...args2];
+    return fn(...combinedArgs);
+  };
 }
 
 /**
